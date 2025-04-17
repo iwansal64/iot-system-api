612b7ef36a0db5301a22b7e8616b66d2d1054314
Iwansal64 <iwansal64@gmail.com>
2025-04-17 08:16:09 +0700

    Add API to activate device

--- errors.ts
+++ errors.ts
@@ -1,4 +1,14 @@
-export const ERROR_APIKEY_INVALID = {
-    message: "Error api-key invalid",
+export const ERROR_UNKNOWN_ERROR = {
+    message: "Unknown error",
     error_code: "000"
 };
+
+export const ERROR_APIKEY_INVALID = {
+    message: "Error api-key invalid",
+    error_code: "001"
+};
+
+export const ERROR_REQUEST_BODY_NOTCOMPLETE = {
+    message: "Request body is not complete",
+    error_code: "002"
+}
